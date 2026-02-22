5e65cca763c8f297845bb0e0df23dbd7c8ac1f56
arken80 <127999670+arken80@users.noreply.github.com>
2026-02-22 07:20:52 +0300

    Create src/model.py with MediVoiceAIModel and DiagnosisPredictor classes

---
+++ src/model.py
@@ -0,0 +1,18 @@
+class MediVoiceAIModel:
+    def __init__(self):
+        pass
+
+    def train_model(self, data):
+        pass
+
+    def predict(self, input_data):
+        pass
+
+
+class DiagnosisPredictor:
+    def __init__(self):
+        self.model = MediVoiceAIModel()
+
+    def predict_diagnosis(self, symptoms):
+        # Here, you would implement the logic to predict the diagnosis based on symptoms.
+        pass
