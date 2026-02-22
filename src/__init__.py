7a621428830b2984785e42d82ec71fa8a718a99c
arken80 <127999670+arken80@users.noreply.github.com>
2026-02-22 07:21:27 +0300

    Create __init__.py file with package imports and exports

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+# Package imports and exports
+
+# Add import statements for the modules you want to expose from this package
+from .module1 import Class1, function1
+from .module2 import Class2
+
+__all__ = ['Class1', 'function1', 'Class2']
